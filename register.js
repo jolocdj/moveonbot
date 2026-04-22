8abcfaccbe37fe5dd30f7b2bedb7ef9103b99f5f
jolocdj <alfredjolodejesus@gmail.com>
2026-04-22 15:10:01 +0800

    update bot

--- register.js
+++ register.js
@@ -51,13 +51,9 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
   try {
     console.log("Registering slash commands...");
 
-    await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.CLIENT_ID,
-        process.env.GUILD_ID,
-      ),
-      { body: commands },
-    );
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
 
     console.log("Slash commands registered successfully.");
   } catch (error) {
